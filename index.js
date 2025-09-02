58be04233ab4b53feabc1a8cbdeb2428589b2c8d
Abdullah Shabir <abdullahabbasi661@gamil.com>
2025-09-02 10:05:08 +0500

    to be continued

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 //Routes
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+})
+//for web 
+app.get('/users', (req, res) => {
+    /*    
+    <ul>
+        <li> Abdullah Abbasi </li>
+    </ul>
+    
+    */
+    const html = `
+   <ul>
+        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+   </ul>
+   `;
+    res.send(html);
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id === id)
+
+    res.json()
+})
 
 app.listen(PORT, () => console.log('Server Started'));
