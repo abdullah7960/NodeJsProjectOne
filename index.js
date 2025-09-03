c6d909c5737fc3ef94333eab5224c463bb2aa9fb
Abdullah Shabir <abdullahabbasi661@gamil.com>
2025-09-03 09:54:49 +0500

    get put post delte patch done

--- index.js
+++ index.js
@@ -24,11 +24,34 @@ app.get('/users', (req, res) => {
     res.send(html);
 })
 
-app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find(user => user.id === id)
-    return res.json(user);
+app
+    .route("/api/users/:id").get('/api/users/:id', (req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id)
+        return res.json(user);
 
+    })
+    .put((req, res) => {
+        return res.json({ status: "pending" });
+    })
+    .delete((req, res) => {
+        return res.json({ status: "pending" });
+    });
+
+app.post('/api/users', (req, res) => {
+    console.log('todo create new user');
+    return res.json({ status: "pending" });
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    console.log('todo create new user');
+    return res.json({ status: "pending" });
+})
+
+
+app.delete('/api/users/:id', (req, res) => {
+    console.log('todo create new user');
+    return res.json({ status: "pending" });
 })
 
 app.listen(PORT, () => console.log('Server Started'));
